bc0abc3e01562b88cfaf8f842c38c483832d5392
Azizbek <tursunov0114@gmail.com>
2021-05-20 16:39:42 +0500

    setting image

--- mysite/settings.py
+++ mysite/settings.py
@@ -132,6 +132,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MEDIA_URL = '/media/'
